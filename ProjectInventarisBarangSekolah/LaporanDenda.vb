ef2d3f62394ca0e0e0a2e417834d92e9901471d2
Aurelia Clara <aureliaclaraxx@gmail.com>
2026-03-18 23:57:49 +0700

    first commit

--- ProjectInventarisBarangSekolah/LaporanDenda.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Laporan_Denda
-    Private Sub Laporan_Denda_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-        Me.ReportViewer1.RefreshReport()
-    End Sub
-End Class
